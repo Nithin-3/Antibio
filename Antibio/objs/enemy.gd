29e77eac01c0c7908dbb3b71d93cc114983dd776
Ninja Nithin <nithin3dev@gmail.com>
2023-10-29 20:01:01 +0530

    emy

--- Antibio/objs/enemy.gd
+++ Antibio/objs/enemy.gd
@@ -2,11 +2,17 @@ extends CharacterBody2D
 signal line(tar:Vector2)
 var HEALTH = 100
 var spre: bool = true
+var bact = preload("res://objs/spans.tscn")
 func  _process(_delta):
+	if spre:
+		spre = false
+		var noad = bact.instantiate()
+		noad.position = global_position
+		get_parent().add_child(noad)
+		$Timer.start()
 	var player = $"../Player"
-	if global_position.distance_to($"../Player".position) < 600 :
+	if global_position.distance_to($"../Player".position) < 600 and global_position.distance_to($"../Player".position) > 6 :
 		var motion = Vector2()
-
 		position += (player.position - position)/100
 		look_at(player.position)
 
@@ -16,3 +22,7 @@ func minus(val:int):
 	if HEALTH <= 0:
 		queue_free()
 		
+
+
+func _on_timer_timeout():
+	spre = true
