abf673be6837230c0024e6f2b82b6256bce6a51f
Ninja Nithin <nithin3dev@gmail.com>
2023-11-02 01:08:25 +0530

    init

---
+++ Antibio/Player/player_2.gd
@@ -0,0 +1,44 @@
+extends CharacterBody2D
+@export var SPEED = 300.0
+@export var JUMP_VELOCITY = -500.0
+var HEALTH = 100.0 
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+#Animation
+@onready var anim = get_node("AnimationPlayer")
+signal bullet(po:Vector2,ang)
+func  _ready():
+	$HealthBar2D.value = HEALTH
+func _physics_process(delta):
+#	look_at(get_global_mouse_position())
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	if Input.is_action_just_pressed("up") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	var direction = Input.get_axis("left", "right")
+	
+	if(direction == -1):
+		get_node("Sprite2D").rotation_degrees -= 3
+	if(direction == 1):
+		get_node("Sprite2D").rotation_degrees += 3
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		anim.play("Idle")
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	move_and_slide()
+	
+	
+	if Input.is_action_just_pressed("fire1"):
+		bullet.emit($Marker2D.global_position,(get_global_mouse_position() - position).normalized())
+func minus(val:float):
+	HEALTH -= val
+	$HealthBar2D.value = HEALTH
+	if HEALTH <=0 :
+		get_tree().paused = true
+	elif HEALTH < 30:
+		$HealthBar2D.tint_progress = Color.RED
+	elif HEALTH < 50:
+		$HealthBar2D.tint_progress = Color.ORANGE
+	else :
+		$HealthBar2D.tint_progress = Color.GREEN
+
---
+++ Antibio/objs/Area2D.gd
@@ -0,0 +1,32 @@
+extends Area2D
+
+@export var speed : int  = 50
+var rand:Vector2 = Vector2(randf_range(-1,1),randf_range(-1,1))
+func destroy():
+	visible = false
+	$AudioStreamPlayer2D.play()
+func _ready():
+	rotation_degrees = randf_range(0,359)
+	$Timer.start()
+func _physics_process(delta):
+	position += rand * delta * speed
+func _on_timer_timeout():
+	queue_free()
+	
+func minus(_val:int,_force:float):
+	destroy()
+
+func _on_audio_stream_player_2d_finished():
+	queue_free()
+
+
+
+func _on_body_entered(body):
+	if str(body)[0] == "B":
+		body.get_parent().destroy()
+		destroy()
+	if "minus" in body:
+		body.minus(0.5)
+		destroy()
+	if str(body)[0] != "E":
+		rand = -rand + global_position.direction_to(body.position)
