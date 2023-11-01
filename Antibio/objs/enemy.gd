7ef5320e67cbda3308bbfbcee3f835b4a3eba8ff
Ninja Nithin <nithin3dev@gmail.com>
2023-11-01 17:39:35 +0530

    Enemy-upt

--- Antibio/objs/enemy.gd
+++ Antibio/objs/enemy.gd
@@ -1,19 +1,26 @@
 extends CharacterBody2D
 var spred:bool = true
 var HEALTH:float = 100.0
-@export var MUTATION:int = 100.0
+@export var MUTATION:int = 100
 var span = preload("res://objs/spans.tscn")
+var mut:bool = true
 #var explosion = preload("res://explotion.tscn")
 
 var rand:Vector2 = Vector2(randf_range(-1,1),randf_range(-1,1))
 func _ready():
+	$Mutation.value = MUTATION
 	$Timer.start()
 	$Connect.Target = self
 func _process(_delta):
 	$Connect.cont($Connect.Target.global_position)
-	if "MUTATION" in $Connect.Target:
+	if "MUTATION" in $Connect.Target and mut:
 		if $Connect.Target.MUTATION > MUTATION:
-			MUTATION += 10 * _delta
+			$Mut.start()
+			mut = false
+			MUTATION += 100 * _delta
+			print("mutation = ",MUTATION)
+			$Mutation.value = MUTATION
+			
 	if spred:
 		rand = Vector2(randf_range(-1,1),randf_range(-1,1))
 		spred = false
@@ -21,9 +28,9 @@ func _process(_delta):
 		var objs = span.instantiate()
 		objs.position = global_position
 		$"..".add_child(objs)
-	if position.distance_to($"../Player".position) < 600 and position.distance_to($"../Player".position) > 110 :
+	if position.distance_to($"../Player".position) < 600 and position.distance_to($"../Player".position) > 90 :
 		$Sprite2D.look_at($"../Player".position)
-		position += ($"../Player".position - position)/500
+		position += ($"../Player".position - position)/300
 		move_and_slide()
 	else :
 		position += rand * 3
@@ -36,3 +43,5 @@ func minus(val:int,force:float):
 		queue_free()
 func _on_timer_timeout():
 	spred = true
+func _on_mut_timeout():
+	mut = true
