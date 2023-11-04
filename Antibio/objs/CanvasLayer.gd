9426c31606ba38d36853ada100bce6fd01817407
Ninja Nithin <nithin3dev@gmail.com>
2023-11-04 18:51:28 +0530

    UPDATE-PAUSE-menu

--- Antibio/objs/CanvasLayer.gd
+++ Antibio/objs/CanvasLayer.gd
@@ -9,3 +9,8 @@ func _on_pause_pressed():
 func _on_resume_pressed():
 	$ColorRect.visible = false
 	get_tree().paused = false
+
+
+func _on_home_pressed():
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://MainMenu.tscn")
