ede15b16a42cd44d676ef0afcd900d0d479295c3
Gopalakrishnan M <gohari78g@gmail.com>
2023-11-05 20:41:43 +0530

    improvements

--- reb/Antibio/Main.gd
+++ reb/Antibio/Main.gd
@@ -3,7 +3,7 @@ extends Button
 
 
 func _on_pressed():
-	get_tree().change_scene_to_file("res://test/test.tscn")
+	get_tree().change_scene_to_file("res://intro_1.tscn")
 
 
 func _on_quit_pressed():
