cf6bf7c78b3fcdaef631dba60c02e74693e07dbb
Ninja Nithin <nithin3dev@gmail.com>
2023-11-05 22:32:06 +0530

    tch

--- reb/Antibio/Aim.gd
+++ reb/Antibio/Aim.gd
@@ -1,6 +1,6 @@
-extends Node2D
+extends TouchScreenButton
 @onready var bg = $Bg
-@onready var touch = $Bg/touch
+@onready var touch = $touch
 @onready var rad = 118
 var pres = false
 signal direct(DIR:Vector2)
@@ -8,7 +8,7 @@ func _process(_delta):
 	if pres:
 		touch.global_position = get_global_mouse_position()
 		if touch.position.distance_to(Vector2(0,0)) > rad:
-			touch.position = touch.position.direction_to(Vector2(0,0)) * -(rad+300)
+			touch.position = touch.position.direction_to(Vector2(0,0)) * -(rad)
 		direct.emit(-1*(touch.position.direction_to(bg.position)))
 	else :
 		touch.position = bg.position
