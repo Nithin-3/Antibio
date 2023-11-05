59a56de315379c5cdecaf38ac34a5404ae0c812a
Gopalakrishnan M <gohari78g@gmail.com>
2023-11-05 14:09:12 +0530

    bg image added and some changes

--- reb/Antibio/test/test.gd
+++ reb/Antibio/test/test.gd
@@ -70,6 +70,8 @@ func loads() -> int:
 		return 1
 	
 func  save(lev:int):
+	if(lev>5):
+		lev = 1
 	var file = FileAccess.open("user://data.nin",FileAccess.WRITE)
 	file.store_var(lev)
 	file.store_var(ENEMYS_COUNT)
